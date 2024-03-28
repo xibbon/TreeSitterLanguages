527bb648e4c478abeeff298f86a3f756ae83b2c9
Miguel de Icaza <miguel@gnome.org>
2024-03-28 16:28:19 -0400

    Fix

--- Sources/TreeSitterGDScriptRunestone/TreeSitterLanguage+Helpers.swift
+++ Sources/TreeSitterGDScriptRunestone/TreeSitterLanguage+Helpers.swift
@@ -4,8 +4,8 @@ import TreeSitterGDScriptQueries
 
 public extension TreeSitterLanguage {
     static var gdscript: TreeSitterLanguage {
-        let highlightsQuery = TreeSitterLanguage.Query(contentsOf: TreeSitterRustQueries.Query.highlightsFileURL)
-        let injectionsQuery = TreeSitterLanguage.Query(contentsOf: TreeSitterRustQueries.Query.injectionsFileURL)
-        return TreeSitterLanguage(tree_sitter_rust(), highlightsQuery: highlightsQuery, injectionsQuery: injectionsQuery, indentationScopes: .gdscript)
+        let highlightsQuery = TreeSitterLanguage.Query(contentsOf: TreeSitterGDScriptQueries.Query.highlightsFileURL)
+        let injectionsQuery = TreeSitterLanguage.Query(contentsOf: TreeSitterGDScriptQueries.Query.injectionsFileURL)
+        return TreeSitterLanguage(tree_sitter_gdscript(), highlightsQuery: highlightsQuery, injectionsQuery: injectionsQuery, indentationScopes: .gdscript)
     }
 }
